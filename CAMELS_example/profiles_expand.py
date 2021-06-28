2e269cb4f0bdca855623c40db8afa35d874b987a
emilymmoser <42976886+emilymmoser@users.noreply.github.com>
2021-06-28 16:07:19 -0400

    Update profiles_expand.py
    Add IDs, h5py redshift, clean names

--- CAMELS_example/profiles_expand.py
+++ CAMELS_example/profiles_expand.py
@@ -5,8 +5,8 @@ sys.path.insert(0,'/home/jovyan/home/illstack/')
 import matplotlib.pyplot as plt
 import illstack as istk
 import mpi4py.rc
-from decimal import Decimal
 import istk_params as params
+import time
 
 istk.init.initialize('istk_params.py')
 
@@ -20,14 +20,13 @@ prof7 = str(sys.argv[7])
 prof8 = str(sys.argv[8])
 prof9 = str(sys.argv[9])
 snap_num= int(sys.argv[10])
-sim=str(sys.argv[11])
+suite=str(sys.argv[11])
 simulation=str(sys.argv[12])
 
-red_dict={'000':6.0,'001':5.0,'002':4.0,'003':3.5,'004':3.0,'005':2.81329,'006':2.63529,'007':2.46560,'008':2.30383,'009':2.14961,'010':2.00259,'011':1.86243,'012':1.72882,'013':1.60144,'014':1.48001,'015':1.36424,'016':1.25388,'017':1.14868,'018':1.04838,'019':0.95276,'020':0.86161,'021':0.77471,'022':0.69187,'023':0.61290,'024':0.53761,'025':0.46584,'026':0.39741,'027':0.33218,'028':0.27,'029':0.21072,'030':0.15420,'031':0.10033,'032':0.04896,'033':0.0}
-z=red_dict[sys.argv[10]]
+z=params.z
 
-print("Simulation:",sim,simulation)
-print("Snapshot:",snap_num, "z=",z)
+print("Simulation:",suite,simulation)
+print("Snapshot:",snap_num, ", z =",z)
 ntile = 3 # controls tiling -- optimal value not yet clear
 
 mlow=params.mass_low
@@ -44,10 +43,8 @@ mp=1.67e-24 #g
 gamma=5./3.
 kb=1.38e-16 #g*cm^2/s^2/K
 
-#prof=[prof1,prof2,prof3,prof4,prof5,prof6,prof7,prof8,prof9]
-#prof=[prof1,prof2,prof3,prof4,prof5,prof6,prof7]
 #prof=[prof1,prof2]
-prof=[prof1,prof2,prof4,prof7] #for batch runs
+prof=[prof1,prof2,prof4,prof7]
 
 volweight=[]
 vals=[]
@@ -77,12 +74,12 @@ for p in prof:
         print("Computing values for unweighted metallicity (metals_uw)")
         part_type='gas'
         
-        if sim=='IllustrisTNG':
+        if suite=='IllustrisTNG':
             field_list=['GFM_Metallicity','Masses','Coordinates']
             gas_particles=istk.io.getparticles(snap_num,part_type,field_list)
             posp=gas_particles['Coordinates']
             val=gas_particles['GFM_Metallicity'] #ratio
-        elif sim=='SIMBA': 
+        elif suite=='SIMBA': 
             field_list=['Metallicity','Masses','Coordinates']
             gas_particles=istk.io.getparticles(snap_num,part_type,field_list)
             posp=gas_particles['Coordinates']
@@ -94,12 +91,12 @@ for p in prof:
     elif p=='metals_gmw':
         print("Computing values for gas-mass-weighted metallicity (metals_gmw)")
         part_type='gas'
-        if sim=='IllustrisTNG':
+        if suite=='IllustrisTNG':
             field_list=['GFM_Metallicity','Masses','Coordinates']
             gas_particles=istk.io.getparticles(snap_num,part_type,field_list)
             posp=gas_particles['Coordinates']
             val=gas_particles['GFM_Metallicity'] #ratio
-        elif sim=='SIMBA': 
+        elif suite=='SIMBA': 
             field_list=['Metallicity','Masses','Coordinates']
             gas_particles=istk.io.getparticles(snap_num,part_type,field_list)
             posp=gas_particles['Coordinates']
@@ -144,12 +141,12 @@ for p in prof:
     elif p=='metals_emm':
         print("Computing values for emission-measure-weighted metallicity (metals_emm)")
         part_type='gas'
-        if sim=='IllustrisTNG':
+        if suite=='IllustrisTNG':
             field_list=['GFM_Metallicity','Masses','Coordinates']
             gas_particles=istk.io.getparticles(snap_num,part_type,field_list)
             posp=gas_particles['Coordinates']
             val=gas_particles['GFM_Metallicity'] #ratio
-        elif sim=='SIMBA': 
+        elif suite=='SIMBA': 
             field_list=['Metallicity','Masses','Coordinates']
             gas_particles=istk.io.getparticles(snap_num,part_type,field_list)
             posp=gas_particles['Coordinates']
@@ -209,7 +206,7 @@ Group_StarMg=star_metal_fractions[:,6]
 Group_StarSi=star_metal_fractions[:,7]
 Group_StarFe=star_metal_fractions[:,8]
 GroupStarMetallicity=halos['GroupStarMetallicity']
-vel=halos['GroupVel'] 
+vel=halos['GroupVel'] #units km/s/a, multiply this by 1/a to get peculiar vel
 GroupVelx=vel[:,0]
 GroupVely=vel[:,1]
 GroupVelz=vel[:,2]
@@ -222,17 +219,26 @@ rh   = halos['Group_R_Crit200']
 R_Crit500=halos['Group_R_Crit500']
 R_Mean200=halos['Group_R_Mean200']
 R_TopHat200=halos['Group_R_TopHat200']
+ID=np.linspace(0,len(posh)-1,len(posh)) #track the IDs of the group catalog
 
 vals=np.array(vals) #here
 volweight=np.array(volweight) #here
 weights=np.array(weights) #here
-print("weights going into cyprof",np.shape(weights))  
-    
-r, val, n, mh, rh, nprofs,GroupFirstSub,sfr,mstar,GroupBHMass,GroupBHMdot,Group_GasH,Group_GasHe,Group_GasC,Group_GasN,Group_GasO,Group_GasNe,Group_GasMg,Group_GasSi,Group_GasFe,GroupGasMetallicity,GroupLen,GroupMass,GroupNsubs,Group_StarH,Group_StarHe,Group_StarC,Group_StarN,Group_StarO,Group_StarNe,Group_StarMg,Group_StarSi,Group_StarFe,GroupStarMetallicity,GroupVelx,GroupVely,GroupVelz,GroupWindMass,M_Crit500,M_Mean200,M_TopHat200,R_Crit500,R_Mean200,R_TopHat200= istk.cyprof.stackonhalos(posp,vals,posh,mh,rh,GroupFirstSub,sfr,mstar,ntile,volweight,weights,mhmin, mhmax,scaled_radius,mass_kind,GroupBHMass,GroupBHMdot,Group_GasH,Group_GasHe,Group_GasC,Group_GasN,Group_GasO,Group_GasNe,Group_GasMg,Group_GasSi,Group_GasFe,GroupGasMetallicity,GroupLen,GroupMass,GroupNsubs,Group_StarH,Group_StarHe,Group_StarC,Group_StarN,Group_StarO,Group_StarNe,Group_StarMg,Group_StarSi,Group_StarFe,GroupStarMetallicity,GroupVelx,GroupVely,GroupVelz,GroupWindMass,M_Crit500,M_Mean200,M_TopHat200,R_Crit500,R_Mean200,R_TopHat200)
+#print("weights going into cyprof",np.shape(weights))  
+
+
+start=time.time()
+r, val, n, mh, rh, nprofs,GroupFirstSub,sfr,mstar,GroupBHMass,GroupBHMdot,Group_GasH,Group_GasHe,Group_GasC,Group_GasN,Group_GasO,Group_GasNe,Group_GasMg,Group_GasSi,Group_GasFe,GroupGasMetallicity,GroupLen,GroupMass,GroupNsubs,Group_StarH,Group_StarHe,Group_StarC,Group_StarN,Group_StarO,Group_StarNe,Group_StarMg,Group_StarSi,Group_StarFe,GroupStarMetallicity,GroupVelx,GroupVely,GroupVelz,GroupWindMass,M_Crit500,M_Mean200,M_TopHat200,R_Crit500,R_Mean200,R_TopHat200,ID= istk.cyprof.stackonhalos(posp,vals,posh,mh,rh,GroupFirstSub,sfr,mstar,ntile,volweight,weights,mhmin, mhmax,scaled_radius,mass_kind,GroupBHMass,GroupBHMdot,Group_GasH,Group_GasHe,Group_GasC,Group_GasN,Group_GasO,Group_GasNe,Group_GasMg,Group_GasSi,Group_GasFe,GroupGasMetallicity,GroupLen,GroupMass,GroupNsubs,Group_StarH,Group_StarHe,Group_StarC,Group_StarN,Group_StarO,Group_StarNe,Group_StarMg,Group_StarSi,Group_StarFe,GroupStarMetallicity,GroupVelx,GroupVely,GroupVelz,GroupWindMass,M_Crit500,M_Mean200,M_TopHat200,R_Crit500,R_Mean200,R_TopHat200,ID)
 r  =np.reshape(r,  (nprofs,istk.params.bins))
 val=np.reshape(val,(len(volweight),nprofs,istk.params.bins)) #here
 n  =np.reshape(n,  (len(volweight),nprofs,istk.params.bins)) #here
 
 #Change name of npz file here
-np.savez(save_direct+sim+'/'+sim+'_'+simulation+'_'+str(z)+'.npz',r=r[0],val=val,n=n,M_Crit200=mh,R_Crit200=rh,nprofs=nprofs,nbins=istk.params.bins,GroupFirstSub=GroupFirstSub,sfr=sfr,mstar=mstar,GroupBHMass=GroupBHMass,GroupBHMdot=GroupBHMdot,Group_GasH=Group_GasH,Group_GasHe=Group_GasHe,Group_GasC=Group_GasC,Group_GasN=Group_GasN,Group_GasO=Group_GasO,Group_GasNe=Group_GasNe,Group_GasMg=Group_GasMg,Group_GasSi=Group_GasSi,Group_GasFe=Group_GasFe,GroupGasMetallicity=GroupGasMetallicity,GroupLen=GroupLen,GroupMass=GroupMass,GroupNsubs=GroupNsubs,Group_StarH=Group_StarH,Group_StarHe=Group_StarHe,Group_StarC=Group_StarC,Group_StarN=Group_StarN,Group_StarO=Group_StarO,Group_StarNe=Group_StarNe,Group_StarMg=Group_StarMg,Group_StarSi=Group_StarSi,Group_StarFe=Group_StarFe,GroupStarMetallicity=GroupStarMetallicity,GroupVelx=GroupVelx,GroupVely=GroupVely,GroupVelz=GroupVelz,GroupWindMass=GroupWindMass,M_Crit500=M_Crit500,M_Mean200=M_Mean200,M_TopHat200=M_TopHat200,R_Crit500=R_Crit500,R_Mean200=R_Mean200,R_TopHat200=R_TopHat200)
+np.savez(save_direct+suite+'/'+suite+'_'+simulation+'_'+str(z)+'.npz',r=r[0],val=val,n=n,M_Crit200=mh,R_Crit200=rh,nprofs=nprofs,nbins=istk.params.bins,GroupFirstSub=GroupFirstSub,sfr=sfr,mstar=mstar,GroupBHMass=GroupBHMass,GroupBHMdot=GroupBHMdot,Group_GasH=Group_GasH,Group_GasHe=Group_GasHe,Group_GasC=Group_GasC,Group_GasN=Group_GasN,Group_GasO=Group_GasO,Group_GasNe=Group_GasNe,Group_GasMg=Group_GasMg,Group_GasSi=Group_GasSi,Group_GasFe=Group_GasFe,GroupGasMetallicity=GroupGasMetallicity,GroupLen=GroupLen,GroupMass=GroupMass,GroupNsubs=GroupNsubs,Group_StarH=Group_StarH,Group_StarHe=Group_StarHe,Group_StarC=Group_StarC,Group_StarN=Group_StarN,Group_StarO=Group_StarO,Group_StarNe=Group_StarNe,Group_StarMg=Group_StarMg,Group_StarSi=Group_StarSi,Group_StarFe=Group_StarFe,GroupStarMetallicity=GroupStarMetallicity,GroupVelx=GroupVelx,GroupVely=GroupVely,GroupVelz=GroupVelz,GroupWindMass=GroupWindMass,M_Crit500=M_Crit500,M_Mean200=M_Mean200,M_TopHat200=M_TopHat200,R_Crit500=R_Crit500,R_Mean200=R_Mean200,R_TopHat200=R_TopHat200,ID=ID)
+
+end=time.time()
+time_elapsed=(end-start)/60.
+f=open('/home/jovyan/home/illstack/CAMELS_example/Batch_Checks/'+suite+'/'+simulation+'_'+str(z)+'.txt','w')
+f.write("This run took %f minutes to run for %i halos \n SearchRad %.2f, profs (1,2,4,7)"%(time_elapsed,nprofs,params.search_radius))
+f.close()
 
